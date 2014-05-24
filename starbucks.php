dbaa890cb041ec03f59d060281a4a4d135159a31
Mark Matanza <mark.matanza@gmail.com>
2014-05-23 21:19:22 -0700

    starbucks menu

---
+++ starbucks.php
@@ -0,0 +1,348 @@
+<?php
+
+$jsonObject = '{
+	"starbucks": [
+		{
+		"id": 0,
+		"Frappuccino": [
+			{
+				"id": 0,
+				"type": "Coffee",
+				"name": "Caffe Vanilla",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 2.95, 
+						"cal": 300
+					},
+					{
+						"size": "grande 16 fl oz", 
+						"price": 3.75, 
+						"cal": 430
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.45, 
+						"cal": 530
+					}
+				]
+			},
+			{
+				"id": 1,
+				"type": "Coffee",
+				"name": "Hazelnut",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": "290"
+					},
+					{
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": "390"
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": "490"
+					}
+				]
+			},
+			{
+				"id": 2,
+				"type": "Coffee",
+				"name": "Caramel Flan",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 4.25, 
+						"cal": 280
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.75, 
+						"cal": 390
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 5.25, 
+						"cal": 490
+					}
+				]
+			},
+			{	
+				"id": 3,
+				"type": "Coffee",
+				"name": "Caramel",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 300
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 410
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 510
+					}
+				]
+			},
+			{	
+				"id": 4,
+				"type": "Coffee",
+				"name": "Mocha",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 280
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 400
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 500
+					}
+				]
+			},
+			{	
+				"id": 5,
+				"type": "Coffee",
+				"name": "Java Chip",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 330
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 460
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 580
+					}
+				]
+			},
+			{	
+				"id": 6,
+				"type": "Coffee",
+				"name": "Coffee",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 2.95, 
+						"cal": 180
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 3.75, 
+						"cal": 240
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.45, 
+						"cal": 350
+					}
+				]
+			},
+			{	
+				"id": 7,
+				"type": "Creme (Coffee-Free)",
+				"name": "Vanilla Bean",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 2.95, 
+						"cal": 280
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 3.75, 
+						"cal": 400
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.45, 
+						"cal": 490
+					}
+				]
+			},
+			{	
+				"id": 8,
+				"type": "Creme (Coffee-Free)",
+				"name": "Strawberries & Creme",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 280
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 380
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 470
+					}
+				]
+			},
+			{	
+				"id": 9,
+				"type": "Creme (Coffee-Free)",
+				"name": "Double Chocolaty Chip",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 300
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 410
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 520
+					}
+				]
+			},
+			{	
+				"id": 10,
+				"type": "Creme (Coffee-Free)",
+				"name": "Strawberries & Creme",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 270
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 3.45, 
+						"cal": 380
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.95, 
+						"cal": 470
+					}
+				]
+			}
+		]},
+		
+		{
+		"id": 1,
+		"Smoothies": [
+			{
+				"id": 0,
+				"type": "",
+				"name": "Strawberry or Orange Mango",
+				"attributes": [
+					{
+						"size": "", 
+						"price": 4.25, 
+						"cal": "270-300"
+					}
+				]
+			}
+		]},
+		
+		{
+		"id": 2,
+		"Expresso": [
+			{
+				"id": 0,
+				"type": "Signature",
+				"name": "Vanilla Macchiato",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 170
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 230
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.75, 
+						"cal": 290
+					}
+				]
+			},
+			{
+				"id": 1,
+				"type": "Signature",
+				"name": "Caramel Macchiato",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 180
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 240
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.75, 
+						"cal": 300
+					}
+				]
+			},
+			{
+				"id": 2,
+				"type": "Signature",
+				"name": "Hazelnut Macchiato",
+				"attributes": [
+					{
+						"size": "tall 12 fl oz", 
+						"price": 3.75, 
+						"cal": 180
+					},
+					{	
+						"size": "grande 16 fl oz", 
+						"price": 4.45, 
+						"cal": 240
+					},
+					{
+						"size": "tall 24 fl oz", 
+						"price": 4.75, 
+						"cal": 300
+					}
+				]
+			}
+		]}
+
+
+]}';
+
+$jsondecode = json_decode($jsonObject, true);
+
+echo '<pre>';
+print_r($jsondecode);
+echo '</pre>';
+?>
